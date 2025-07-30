541dd27dc21da1324df6285eee52652ca559cc39
S22102001 <Shahar.girtler@gmail.com>
2025-07-31 00:03:43 +0300

    updating role
    updating someone who has rank 4 from regular to expert, and postman

--- AdminSide/server/controllers/userController.js
+++ AdminSide/server/controllers/userController.js
@@ -47,9 +47,25 @@ const getUsersWithRank4 = async (req, res) => {
     }
 };
 
+const upgradeToExpert = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.id,
+            { role: 'expert' },
+            { new: true }
+        );
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        res.status(200).json({ message: 'User upgraded to expert', user });
+    } catch (error) {
+        res.status(500).json({ message: 'Error upgrading user', error });
+    }
+};
+
 
 module.exports = {
     getAllUsers,
     loginUser,
-    getUsersWithRank4
+    getUsersWithRank4,
+    upgradeToExpert
 };
--- AdminSide/server/routers/userrouter.js
+++ AdminSide/server/routers/userrouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, loginUser, getUsersWithRank4 } = require('../controllers/userController');
+const { getAllUsers, loginUser, getUsersWithRank4, upgradeToExpert } = require('../controllers/userController');
 
 router.get('/', getAllUsers);
 router.post('/login', loginUser);
 router.get('/rank4', getUsersWithRank4);
-
+router.put('/upgrade/:id', upgradeToExpert);
 
 module.exports = router;
